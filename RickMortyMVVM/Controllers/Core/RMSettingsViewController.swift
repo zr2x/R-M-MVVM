b97db2660faf2da5681bb24faa2ba24036747069
Iskander Sitdikov <master5212345@gmail.com>
2024-05-11 18:18:57 +0500

    added SwiftUI view

--- RickMortyMVVM/Controllers/Core/RMSettingsViewController.swift
+++ RickMortyMVVM/Controllers/Core/RMSettingsViewController.swift
@@ -6,20 +6,37 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class RMSettingsViewController: UIViewController {
-    
-    private let viewModel = RMSettingsViewViewModel(
-        cellViewModels: RMSettingsOption.allCases.compactMap({
-            return RMSettingsCellViewModel(type: $0)
-        }))
+
+    private let settingsSwiftUIController = UIHostingController(
+        rootView: RMSettingsView(
+            viewModel: RMSettingsViewViewModel(
+                cellViewModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0)
+    }))))
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         self.title = "Settings"
+        addSwiftUIController()
     }
 
-
+    private func addSwiftUIController()  {
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            settingsSwiftUIController.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 }
---
+++ RickMortyMVVM/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,43 @@
+//
+//  RMSettingsView.swift
+//  RickMortyMVVM
+//
+//  Created by Искандер Ситдиков on 11.05.2024.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainterColor))
+                        .cornerRadius(10)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 5)
+            }
+            .padding(.bottom, 3)
+        }
+    }
+}
+
+#Preview {
+    RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0)
+    })))
+}
